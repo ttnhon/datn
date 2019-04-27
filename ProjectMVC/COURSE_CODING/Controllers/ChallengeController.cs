cff05d6a6089ef1cdd0dc307bf7cc9b27348ae73
DESKTOP-VIIL54U\phiph <phiphuc27@gmail.com>
2019-04-28 00:48:11 +0700

    fix view challenge (add controller)

---
+++ ProjectMVC/COURSE_CODING/Controllers/ChallengeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using COURSE_CODING.Models;
+using DAO.DAO;
+
+namespace COURSE_CODING.Controllers
+{
+    public class ChallengeController : Controller
+    {
+        // GET: Challenge
+        public ActionResult Problem()
+        {
+            ChallengeModel model = new ChallengeModel()
+            {
+                Title = "Input and Output",
+                Description = "In this challenge, we're practicing reading input from stdin and printing output to stdout.\n"
+                + "In C++, you can read a single whitespace-separated token of input using cin, and print output to stdout using cout. For example, let's say we declare the following variables:\n"
+                + "string s;\nint n;\n"
+                + ".....\n"
+                + "Task\n Read 3 numbers from stdin and print their sum to stdout.",
+                InputFormat = "A single line containing 3 space-separated integers: a, b, and c.",
+                OutputFormat = "Print the sum of the three numbers on a single line.",
+                ChallengeDifficulty = 0,
+                Constraints = "",
+                Score = 5,
+
+            };
+            return View(model);
+        }
+    }
+}
---
+++ ProjectMVC/COURSE_CODING/Models/ChallengeModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DAO.EF;
+
+namespace COURSE_CODING.Models
+{
+    public class ChallengeModel
+    {
+        public int ID { get; set; }
+        public int OwnerID { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public string InputFormat { get; set; }
+        public string OutputFormat { get; set; }
+        public int ChallengeDifficulty { get; set; }
+        public string Constraints { get; set; }
+        public int TimeDo { get; set; }
+        public int Score { get; set; }
+        public string Solution { get; set; }
+        public string Tags { get; set; }
+
+        public List<LANGUAGE_CODE> languages = new List<LANGUAGE_CODE>();
+    }
+}
