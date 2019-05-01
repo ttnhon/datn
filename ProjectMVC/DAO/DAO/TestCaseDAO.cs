7af1ecb98458a13ef4c94dcfa1b91d82a8e8ed85
dvp113 <37408421+dvp113@users.noreply.github.com>
2019-05-01 20:22:50 +0700

    build runcode + submitcode challenge (base)

---
+++ ProjectMVC/DAO/DAO/TestCaseDAO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DAO.EF;
+
+namespace DAO.DAO
+{
+    public class TestCaseDAO
+    {
+        DCCourseCoding db = null;
+        public TestCaseDAO()
+        {
+            db = new DCCourseCoding();
+        }
+
+        /// <summary>
+        /// Get all test case of 1 challenge
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public List<TESTCASE> GetAllByChallengeID(int challengeID)
+        {
+            return db.TESTCASES.Where(item => item.ChallengeID == challengeID).ToList();
+        }
+
+        /// <summary>
+        /// Get first test case of 1 challenge
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public TESTCASE GetOneByChallengeID(int challengeID)
+        {
+            return db.TESTCASES.FirstOrDefault(item => item.ChallengeID == challengeID);
+        }
+    }
+}
