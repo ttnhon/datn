f80308fd7bb2b40d39371975ded4a22daec08065
REFICUL\Reficul <ttnhon0404@gmail.com>
2019-05-04 20:59:20 +0700

    add page Manage Challenges (moderator dashboard)

---
+++ ProjectMVC/COURSE_CODING/Models/ManageChallengesModel.cs
@@ -0,0 +1,13 @@
+﻿using DAO.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COURSE_CODING.Models
+{
+    public class ManageChallengesModel
+    {
+        public List<CHALLENGE> lsChallenges { get; set; }
+    }
+}
