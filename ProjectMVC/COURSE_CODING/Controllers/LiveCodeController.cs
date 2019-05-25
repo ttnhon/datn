e147d2aae9a8f2d166a49ea92c19e38f471e72bf
dvp113 <37408421+dvp113@users.noreply.github.com>
2019-05-26 00:21:36 +0700

    Add LiveCode

---
+++ ProjectMVC/COURSE_CODING/Controllers/LiveCodeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COURSE_CODING.Controllers
+{
+    public class LiveCodeController : Controller
+    {
+        // GET: LiveCode
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
