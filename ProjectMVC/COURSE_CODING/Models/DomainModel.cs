d6f68a54634005a393c78eb54ce8a65aab4580ee
PHUC-PC\dohongphuc <dohongphuc1997@gmail.com>
2019-07-07 13:05:07 +0700

    filter domain/index

--- ProjectMVC/COURSE_CODING/Models/DomainModel.cs
+++ ProjectMVC/COURSE_CODING/Models/DomainModel.cs
@@ -9,7 +9,8 @@ namespace COURSE_CODING.Models
     public class DomainModel
     {
         public LANGUAGE Language { get; set; }
-        public List<CHALLENGE> List = new List<CHALLENGE>();
+        public List<CHALLENGE> ListSolved = new List<CHALLENGE>();
+        public List<CHALLENGE> ListUnsolved = new List<CHALLENGE>();
         public string status { get; set; }
         public string difficulty { get; set; }
     }
